3c49736e8aa6d33bf2838f660f33d95851230546
Micha Vie <micha@vleap.ai>
2026-08-13 22:37:32 +0200

    update crm outreach

--- warps/outreach/i18n/index.ts
+++
@@ -1,9 +0,0 @@
-import { de } from './de'
-import { en } from './en'
-import { es } from './es'
-import { fr } from './fr'
-import { ro } from './ro'
-
-export type Translations = typeof en
-
-export const translations: Record<string, Translations> = { en, de, fr, es, ro }
